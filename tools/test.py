7124150d298f2271660cee3edb3e096e6572649b
nicekwell <nicekwell@gmail.com>
2016-12-22 23:08:05 +0800

    添加tools。

---
+++ tools/test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import serial
+import time
+
+gr3e = serial.Serial('/dev/ttyUSB0', baudrate = 115200, bytesize = 8, parity = 'N', stopbits = 1, timeout = 1)
+
+while True:
+    time.sleep(0.1)
+    gr3e.flushInput()
+    getdata = list(gr3e.read(8))
+#    print(getdata)
+#    print(getdata.index(1))
+    datafinal = getdata[getdata.index(1):getdata.index(1)+4]
+    print(datafinal)
+    
+    
+    
+      
